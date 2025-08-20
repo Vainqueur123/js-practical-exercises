d62d8b5d6d0f2e11a2fa29fe7df2dc3cc91b8ee7
Vainqueur123 <ndamukundavainqueur@gmail.com>
2025-08-20 10:34:27 +0200

    predict the answer and state why

---
+++ myNumber.js
@@ -0,0 +1,11 @@
+// What would be the output of the following code and why
+let myNumber = 0;
+if (myNumber = 10) {
+	console.log('Hacked!')
+}
+console.log(myNumber)
+
+/*
+The out ptu is 10 and hacked because we use let by set value of myNumber to 0 at initial point 
+then we reassign it in condition that is why we will get 10 and Hacked! at output
+ */
